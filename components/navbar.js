3ffcc08ba7828adaede9a956d0fd205160c7206c
Hansanca <hansanca@gmail.com>
2023-01-29 13:00:24 -0500

    test

---
+++ components/navbar.js
@@ -0,0 +1,33 @@
+import react from 'react';
+
+const Navbar = () => {
+    
+    return (
+        <>
+            <div className='navdiv'>
+
+            <h1 className='header'>
+
+                Crypto Simulator:
+
+            </h1>
+
+            <h4 calssName='total-value'>
+
+                Total value:
+
+            </h4>
+
+            <h4 className='funds-available'>
+
+                Funds Available:
+
+            </h4>
+
+            </div>
+        </>
+    )
+}
+
+
+export default Navbar;
